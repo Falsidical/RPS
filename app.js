94a21c59349fa9bc84a7f504fab5483898913831
Falsidical <franport@outlook.com>
2024-11-18 07:29:27 +0000

    Finish game logic

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const humanScoreLabel = document.querySelector('#humanScore');
-const computerScoreLabel = document.querySelector('#computerScore');
+const powerIndicator = document.querySelector('#powerIndicator');
+const btnSound = document.querySelector('audio[data-sound="btn"]');
 const buttons = document.querySelectorAll('button');
 // 0,1,2 = RPS, 3=start, 4=reset, 5=log, 6=off
 
-const btnSound = document.querySelector('audio[data-sound="btn"]');
-const powerIndicator = document.querySelector('#powerIndicator');
-
-const gameStatusLabel = document.querySelector('#gameStatusLabel');
-const screenDescLabel = document.querySelector('#screenDesc');
+const screenTopText = document.querySelector('#gameStatusLabel');
+const screenBotText = document.querySelector('#screenDesc');
 const playerImg = document.querySelector('#playerImg');
 const computerImg = document.querySelector('#computerImg');
 
-let showLogs = false;
-let systemStatus = 0;
-let humanChoice;
-let computerChoice;
-let humanScore = 0;
-let computerScore = 0;
-let round = 0;
+const roundCounterLabel = document.querySelector('#roundCounter');
+const humanScoreLabel = document.querySelector('#humanScore');
+const computerScoreLabel = document.querySelector('#computerScore');
+
+let systemStatus = 'off';
+let currentRound;
+let humanScore;
+let computerScore;
 
 buttons.forEach((btn) =>
   btn.addEventListener('click', () => {
@@ -26,72 +24,85 @@ buttons.forEach((btn) =>
   })
 );
 
-function updateScore() {
+function updateScreenScore() {
+  roundCounterLabel.innerText = currentRound;
   humanScoreLabel.innerText = humanScore;
   computerScoreLabel.innerText = computerScore;
 }
 
 for (let i = 0; i < 3; i++) {
   buttons[i].addEventListener('click', (e) => {
-    if (systemStatus === 0 || systemStatus === 3) return;
-    humanChoice = buttons[i].dataset.action;
-    playerImg.setAttribute('src', `img/${humanChoice}.png`);
-    playRound();
+    if (systemStatus !== 'playing') return;
+    playRound(buttons[i].dataset.action);
   });
 }
 
-buttons[3].addEventListener('click', () => {
-  if (systemStatus) return;
-  powerIndicator.classList.add('on');
-  updateScore();
-  gameStatusLabel.innerText = 'Game Active. Make a decision.';
-  systemStatus = 1;
-});
-
+buttons[3].addEventListener('click', turnOn);
+buttons[4].addEventListener('click', playGame);
 buttons[6].addEventListener('click', turnOff);
+
+function turnOn() {
+  if (!systemStatus === 'off') return;
+  powerIndicator.classList.add('on');
+  playGame();
+}
 
 function turnOff() {
   powerIndicator.classList.remove('on');
-  systemStatus = 0;
-  gameStatusLabel.innerText = '';
-  screenDescLabel.innerText = '';
+  systemStatus = 'off';
+  screenTopText.innerText = '';
+  screenBotText.innerText = '';
   humanScoreLabel.innerText = '';
   computerScoreLabel.innerText = '';
   computerImg.removeAttribute('src');
   playerImg.removeAttribute('src');
 }
 
-const hands = ['rock', 'paper', 'scissors'];
+function playGame() {
+  humanScore = 0;
+  computerScore = 0;
+  currentRound = 1;
 
-function getComputerChoice() {
-  return hands[Math.floor(Math.random() * 3)];
+  updateScreenScore();
+  screenTopText.innerText = 'GAME STARTED';
+  screenBotText.innerText = 'MAKE A DECISION';
+  systemStatus = 'playing';
 }
 
-function playRound() {
+function getComputerChoice() {
+  return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+}
+
+function playRound(humanChoice) {
   computerChoice = getComputerChoice();
+  playerImg.setAttribute('src', `img/${humanChoice}.png`);
+  computerImg.setAttribute('src', `img/${computerChoice}.png`);
+  screenBotText.innerText = 'ROUND OVER. PICK A NEW HAND';
+
   if (humanChoice === computerChoice) {
-    gameStatusLabel.innerText = 'Its a tie!';
+    screenTopText.innerText = 'Its a tie';
   } else if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === 'scissors' && computerChoice === 'paper')) {
     console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    gameStatusLabel.innerText = 'Human won';
+    screenTopText.innerText = 'You won this round';
     humanScore++;
   } else {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    gameStatusLabel.innerText = 'Computer won';
+    screenTopText.innerText = 'Computer won this round';
     computerScore++;
   }
-  computerImg.setAttribute('src', `img/${computerChoice}.png`);
-  updateScore();
-  round++;
 
-  if (round === 5) {
-    systemStatus = 3;
-    gameStatusLabel.innerText = humanScore > computerScore ? 'HUMAN WON' : 'COMPUTER WON';
-    screenDescLabel.innerText = 'PRESS START TO START A NEW GAME';
-    computerImg.removeAttribute('src');
-    playerImg.removeAttribute('src');
+  updateScreenScore();
+
+  if (currentRound === 5) {
+    console.log('end round' + currentRound);
+    systemStatus = 'gameOver';
+    if (humanScore === computerScore) {
+      screenTopText.innerText = `IT'S A TIE. ${humanScore} TO ${computerScore}`;
+    } else {
+      screenTopText.innerText = humanScore > computerScore ? `HUMAN WON ${humanScore} TO ${computerScore}` : `COMPUTER WON ${computerScore} to ${humanScore}`;
+    }
+
+    screenBotText.innerText = 'PRESS START TO START A NEW GAME';
   } else {
-    systemStatus = 2;
-    screenDescLabel.innerText = 'MAKE A CHOICE TO CONTINUE';
+    currentRound++;
   }
 }
